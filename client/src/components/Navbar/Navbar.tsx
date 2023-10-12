b074b671ebcfdd660176a9a376e7e3220f4f85e8
DenisLoo <d.lubenko@gmail.com>
2023-10-12 16:29:02 +0300

    pre Activities styles

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,61 +2,93 @@ import React, { useEffect, useState } from 'react';
 import { IPropsLogin, IPropsNavbar } from '../../types/types';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
 import { fetchUserLogout } from '../../store/userSlice/thunkUser';
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Container, Nav, Navbar, Offcanvas } from 'react-bootstrap';
-import styles from './navbar.module.css'
+import styles from './navbar.module.css';
 
 export default function NavBar(): JSX.Element {
   const { user } = useAppSelector((store) => store.user);
+  const { profile } = useAppSelector((store) => store.profile);
   console.log('user', user);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-// export default function NavBar({ user, setUser }: IPropsNavbar): JSX.Element {
-//   console.log(user.login);
+  // export default function NavBar({ user, setUser }: IPropsNavbar): JSX.Element {
+  //   console.log(user.login);
 
   const logoutHandler = async (): Promise<void> => {
     await dispatch(fetchUserLogout());
     navigate('/');
-  }
+  };
 
   return (
     <>
-    {['xxl'].map((expand) => (
+      {['xxl'].map((expand) => (
         <Navbar key={expand} expand={expand}>
-          <Container className={styles.navbar} >
-            <Link className={styles.navlink} to='/'>Sport Mate</Link>
+          <Container className={styles.navbar}>
+            <Link className={styles.navlink} to="/">
+              Что?Где?Спорт!
+            </Link>
             <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
             <Navbar.Offcanvas
               id={`offcanvasNavbar-expand-${expand}`}
               aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
               placement="end"
               className={styles.navlink}
-              
-              
             >
               <Offcanvas.Header aria-controls={`offcanvasNavbar-expand-${expand}`} closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`} className={styles.navlink}>
+                <Offcanvas.Title
+                  id={`offcanvasNavbarLabel-expand-${expand}`}
+                  className={styles.navlink}
+                >
                   Меню
                 </Offcanvas.Title>
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                {user?.login ? ( 
+                  {user?.login ? (
                     <>
-                      <Link className={styles.navlink} to="/activities">Активности</Link>
-                      <Link className={styles.navlink} to="/locations">Локации</Link>
-                      <Link className={styles.navlink} to="/userLC">Личный кабинет</Link>
-                      <Link className={styles.navlink} onClick={logoutHandler} to='/'>Выйти</Link>
-                    </> 
+                   
+                        <Link className={styles.navlink} to="/activities">
+                          Активности
+                        </Link>
+                        <Link className={styles.navlink} to="/locations">
+                          Локации
+                        </Link>
+                        <Link className={styles.navlink} to="/userLC">
+                          Личный кабинет
+                        </Link>
+                        <Link className={styles.navlink} onClick={logoutHandler} to="/">
+                          Выйти
+                        </Link>
+                   
+                      <div className={styles.profile_card_nav__content}>
+                        {user?.Profile && (
+                          <img
+                            className={styles.profile_card_nav__avatar}
+                            src={`/public/photos/${user?.Profile?.user_photo}`}
+                            alt="avatar"
+                          />
+                        )}
+
+                        <div className={styles.profile_card_nav__user}>
+                          {user?.Profile?.user_name}
+                        </div>
+                      </div>
+                    </>
                   ) : (
                     <>
-                      <Link className={styles.navlink} to="/activities">Активности</Link>
-                      <Link className={styles.navlink} to="/locations">Локации</Link>
-                      <Link className={styles.navlink} to="/access">Войти</Link>
-                    </>) 
-                }
-                  
+                      <Link className={styles.navlink} to="/activities">
+                        Активности
+                      </Link>
+                      <Link className={styles.navlink} to="/locations">
+                        Локации
+                      </Link>
+                      <Link className={styles.navlink} to="/access">
+                        Войти
+                      </Link>
+                    </>
+                  )}
                 </Nav>
               </Offcanvas.Body>
             </Navbar.Offcanvas>
@@ -65,7 +97,8 @@ export default function NavBar(): JSX.Element {
       ))}
     </>
   );
-     {/* <Navbar className={styles.navbar} bg="light" data-bs-theme="light">
+  {
+    /* <Navbar className={styles.navbar} bg="light" data-bs-theme="light">
         <Container>
         <Navbar.Brand className={styles.navlink} href="/">Sport Mate</Navbar.Brand>
           <Nav className={styles.nav}>
@@ -92,7 +125,8 @@ export default function NavBar(): JSX.Element {
           )}
           </Nav>
         </Container>
-      </Navbar> */}
-    // </>
+      </Navbar> */
+  }
+  // </>
   // );
 }
