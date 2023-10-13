2af4df7f47aeed85f6c50f2ac91fe25f87070d47
nzhirkova <nzhirk@gmail.com>
2023-10-13 17:39:26 +0600

    edit style LC

--- client/src/components/ProfileCard/ProfileCard.tsx
+++ client/src/components/ProfileCard/ProfileCard.tsx
@@ -27,7 +27,7 @@ export default function ProfileCard() {
   };
 
   return (
-      <Card className={styles.profile_card__wrapper} bg="transparent" >
+      <Card className={styles.profile_card__wrapper}>
         <Card.Img className={styles.profile_card__avatar}
         variant="top" 
         src={`/public/photos/${user?.Profile?.user_photo}`} 
@@ -37,7 +37,9 @@ export default function ProfileCard() {
           onSubmit={submitHandler}
           className={styles.profile_card_avatar__form} >
           <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label styles={{ fontSize: '16px' }}>Добавить фото профиля</Form.Label>
+            <Form.Label className={styles.profile_add_photo}>
+              Добавить фото профиля
+            </Form.Label>
             <Form.Control type="file" name="user_photo" onChange={changeFileHandler} />
             <Button variant="secondary" type="submit" className={styles.profile_form__button}>
             Сохранить
@@ -51,15 +53,15 @@ export default function ProfileCard() {
             Обо мне:
             {' '}{user?.Profile?.user_about}
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item className={styles.profile_card__userabout_age}>
             Возраст:
             {' '}{user?.Profile?.user_age}
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item className={styles.profile_card__userabout_age}>
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M280-40q-33 0-56.5-23.5T200-120v-720q0-33 23.5-56.5T280-920h400q33 0 56.5 23.5T760-840v720q0 33-23.5 56.5T680-40H280Zm0-200v120h400v-120H280Zm200 100q17 0 28.5-11.5T520-180q0-17-11.5-28.5T480-220q-17 0-28.5 11.5T440-180q0 17 11.5 28.5T480-140ZM280-320h400v-400H280v400Zm0-480h400v-40H280v40Zm0 560v120-120Zm0-560v-40 40Z"/></svg>
            {' '}{user?.Profile?.user_mobile}
           </ListGroup.Item>
-          <ListGroup.Item>
+          <ListGroup.Item className={styles.profile_card__userabout_tg}>
              <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="28px" height="28px"><path fill="#29b6f6" d="M24 4A20 20 0 1 0 24 44A20 20 0 1 0 24 4Z"/><path fill="#fff" d="M33.95,15l-3.746,19.126c0,0-0.161,0.874-1.245,0.874c-0.576,0-0.873-0.274-0.873-0.274l-8.114-6.733 l-3.97-2.001l-5.095-1.355c0,0-0.907-0.262-0.907-1.012c0-0.625,0.933-0.923,0.933-0.923l21.316-8.468 c-0.001-0.001,0.651-0.235,1.126-0.234C33.667,14,34,14.125,34,14.5C34,14.75,33.95,15,33.95,15z"/><path fill="#b0bec5" d="M23,30.505l-3.426,3.374c0,0-0.149,0.115-0.348,0.12c-0.069,0.002-0.143-0.009-0.219-0.043 l0.964-5.965L23,30.505z"/><path fill="#cfd8dc" d="M29.897,18.196c-0.169-0.22-0.481-0.26-0.701-0.093L16,26c0,0,2.106,5.892,2.427,6.912 c0.322,1.021,0.58,1.045,0.58,1.045l0.964-5.965l9.832-9.096C30.023,18.729,30.064,18.416,29.897,18.196z"/></svg>
            {' '}<a href={user?.Profile?.user_tg} className={styles.profile_card_tg_link}>{user?.Profile?.user_tg}</a>
           </ListGroup.Item>
